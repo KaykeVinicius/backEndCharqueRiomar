23a39c26b7e64eefbd7b6da2f112482df797c7ef
Kayke-V <kaykejumaentregas@gmail.com>
2025-10-03 12:20:19 -0400

    feat: removendo usuarios

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  belongs_to :tipo_usuario
-  
-  validates :cpf, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-end
--- app/serializers/user_serializer.rb
+++
@@ -1,4 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :descricao, :cpf, :password_digest, :email
-  has_one :tipo_usuario
-end
